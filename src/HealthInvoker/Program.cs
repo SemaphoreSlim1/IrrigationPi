11b571c06a49389bb3939bc3e472a4830b91f792
Matthew Thomas <matthewethomas@users.noreply.github.com>
2020-11-24 01:30:02 -0600

    3 add healthchecks (#24)
    * Adding health invoker app
    * Don't generate coverage metrics on pull request builds
    * Let's not deploy for pull request builds

---
+++ src/HealthInvoker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HealthInvoker
+{
+    [ExcludeFromCodeCoverage]
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            using (var cli = new HttpClient())
+            {
+                cli.BaseAddress = new Uri("http://localhost/");
+
+                try
+                {
+                    var response = await cli.GetAsync("Health");
+                    return response.IsSuccessStatusCode ? 0 : 1;
+                }
+                catch
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+}
