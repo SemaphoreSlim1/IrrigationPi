17745557cbd7c634fd1342841a7164d8d14a073d
Matthew Thomas <matthew.e.thomas@outlook.com>
2020-11-24 01:03:25 -0600

    Adding health invoker app

---
+++ src/HealthInvoker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HealthInvoker
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            using (var cli = new HttpClient())
+            {
+                cli.BaseAddress = new Uri("http://localhost/");
+
+                try
+                {
+                    var response = await cli.GetAsync("");
+                    return response.IsSuccessStatusCode ? 0 : 1;
+                }
+                catch
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+}
